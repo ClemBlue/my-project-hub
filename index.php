f9d71d8ad6ebccbe699d90303c2a1f67a554c340
ClemBlue <clement.blin76@gmail.com>
2023-10-25 14:50:58 +0200

    Début de la création du hub

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Mon Site Hub</title>
+</head>
+<body>
+    <header>
+        <h1>Bienvenue sur Mon Site Hub</h1>
+    </header>
+    <nav>
+        <ul>
+            <li><a href="index.php">Accueil</a></li>
+            <li><a href="cv.php">CV</a></li>
+            <li><a href="">Mes réalisations</a></li>
+        </ul>
+    </nav>
+    <main>
+        <section>
+            <h2>À Propos de Moi</h2>
+            <p>Je m'appelle [Votre Nom]. Voici quelques-unes de mes passions :</p>
+
+            <div class="passion-block">
+                <h3>Jeux Vidéo</h3>
+                <p>J'aime les jeux vidéo parce que [expliquez pourquoi vous les aimez, par exemple : "ils me permettent de m'évader dans des mondes fantastiques"].</p>
+            </div>
+
+            <div class="passion-block">
+                <h3>Manga</h3>
+                <p>J'aime les mangas parce que [expliquez pourquoi vous les aimez, par exemple : "je trouve les histoires captivantes et les dessins incroyables"].</p>
+            </div>
+        </section>
+    </main>
+    <footer>
+        <p>&copy; <?php echo date("Y"); ?> Mon Site Hub. Tous droits réservés.</p>
+    </footer>
+</body>
+</html>
