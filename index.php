56741e838d23dc54da783649684114d134844df0
ClemBlue <clement.blin76@gmail.com>
2023-10-30 15:25:49 +0100

    Ajout section pour liste des mini sites

--- index.php
+++ index.php
@@ -1,9 +1,29 @@
 <?php
     require('includes/envoyer_email.php');
     include('includes/header.php');
+
+    $miniSites = [
+        ['nom' => 'Mini-Site 1', 'description' => 'Description du mini-site 1', 'lien' => 'lien_vers_mini_site_1', 'image' => 'lien de l\'image'],
+        ['nom' => 'Mini-Site 2', 'description' => 'Description du mini-site 2', 'lien' => 'lien_vers_mini_site_2', 'image' => 'lien de l\'image'],
+    ];
+    
+
 ?>
     <section class="intro">
         <p>Je m'appelle Clément et je suis passionné par la création de sites web. Sur ce site, vous trouverez mon CV ainsi que plusieurs projets sur lesquels j'ai travaillé ou sur lesquels je travaille actuellement. J'aime utiliser diverses technologies pour diversifier mes compétences.</p>
+    </section>
+
+    <section>
+        <ul>
+            <?php foreach ($miniSites as $miniSite) : ?>
+                <li alt="<?php echo $miniSite['image'] ?>">
+                    <a href="<?php echo $miniSite['lien']; ?>">
+                        <h2><?php echo $miniSite['nom']; ?></h2>
+                    </a>
+                    <p><?php echo $miniSite['description']; ?></p>
+                </li>
+            <?php endforeach; ?>
+        </ul>
     </section>
 
     <section class="contact">
