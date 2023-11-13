477e37446bd812c9e47702168490a9600d847504
ClemBlue <clement.blin76@gmail.com>
2023-11-13 14:42:46 +0100

    Personnalisation du footer et correction de quelques problèmes de chemin

--- includes/footer.php
+++ includes/footer.php
@@ -1,7 +1,9 @@
         <footer class="mt-auto">
-            <p>Merci de visiter mon site ! N'hésitez pas à regarder mon dépôt Git ou mon Linkedin :</p>
-            <a href="www.linkedin.com/in/clément-blin" target="_blank"><img src="assets/images/linkedin.png" alt="LinkedIn"></a>
-            <a href="https://github.com/ClemBlue" target="_blank"><img src="assets/images/github.png" alt="GitHub"></a>
+            <p>Merci de visiter mon site ! N'hésitez pas à regarder mon dépôt Git ou mon Linkedin</p>
+            <div class="liens">
+                <a href="https://linkedin.com/in/clément-blin" target="_blank"><img src="/Perso/ProjetPerso/my-project-hub/images/linkedin.png" alt="LinkedIn"></a>
+                <a href="https://github.com/ClemBlue" target="_blank"><img src="/Perso/ProjetPerso/my-project-hub/images/github.png" alt="GitHub"></a>
+            </div>
         </footer>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
         <script type="text/javascript" src="./assets/js/script.js"></script>
--- includes/header.php
+++ includes/header.php
@@ -5,23 +5,23 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Accueil - Votre Nom</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-        <link rel="stylesheet" type="text/css" href="./assets/css/style.css">
+        <link rel="stylesheet" type="text/css" href="/Perso/ProjetPerso/my-project-hub/assets/css/style.css">
     </head>
     <body class="d-flex flex-column min-vh-100">
         <header>
             <nav class="navbar navbar-expand-lg">
                 <div class="container-fluid">
-                    <a class="navbar-brand" href="#">Clément Blin</a>
+                    <a class="navbar-brand" href="/Perso/ProjetPerso/my-project-hub/index.php">Clément Blin</a>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                     </button>
                     <div class="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                             <li class="nav-item">
-                                <a class="nav-link active" aria-current="page" href="#">Accueil</a>
+                                <a class="nav-link active" aria-current="page" href="/Perso/ProjetPerso/my-project-hub/index.php">Accueil</a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="./pages/cv.php">CV</a>
+                                <a class="nav-link" href="/Perso/ProjetPerso/my-project-hub/pages/cv.php">CV</a>
                             </li>
                             <li class="nav-item dropdown">
                                 <a class="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false">
