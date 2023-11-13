25005566a2f1adb6cf6d33a84ed35cf3a6eff052
ClemBlue <clement.blin76@gmail.com>
2023-11-13 11:15:17 +0100

    Mise en form du header et de la navbar

--- includes/footer.php
+++ includes/footer.php
@@ -1,4 +1,4 @@
-        <footer>
+        <footer class="mt-auto">
             <p>Merci de visiter mon site ! N'hésitez pas à regarder mon dépôt Git ou mon Linkedin :</p>
             <a href="www.linkedin.com/in/clément-blin" target="_blank"><img src="assets/images/linkedin.png" alt="LinkedIn"></a>
             <a href="https://github.com/ClemBlue" target="_blank"><img src="assets/images/github.png" alt="GitHub"></a>
--- includes/header.php
+++ includes/header.php
@@ -7,7 +7,36 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
         <link rel="stylesheet" type="text/css" href="./assets/css/style.css">
     </head>
-    <body>
+    <body class="d-flex flex-column min-vh-100">
         <header>
-            <h1>Bienvenue sur mon site !</h1>
+            <nav class="navbar navbar-expand-lg">
+                <div class="container-fluid">
+                    <a class="navbar-brand" href="#">Clément Blin</a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li class="nav-item">
+                                <a class="nav-link active" aria-current="page" href="#">Accueil</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="./pages/cv.php">CV</a>
+                            </li>
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    Mes sites
+                                </a>
+                                <ul class="dropdown-menu">
+                                    <li><a class="dropdown-item disabled" aria-disabled="true" href="#">Fate /</a></li>
+                                    <li><hr class="dropdown-divider"></li>
+                                    <li><a class="dropdown-item disabled" aria-disabled="true" href="#">Nier Automata</a></li>
+                                    <li><hr class="dropdown-divider"></li>
+                                    <li><a class="dropdown-item disabled" aria-disabled="true" href="#">Genshin Impact</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
         </header>
